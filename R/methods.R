749f0bd972afd4937d1f8fb156b7765798821e54
HughParsonage <hugh.parsonage@gmail.com>
2019-04-23 23:59:44 +1000

    Add + method

---
+++ R/methods.R
@@ -0,0 +1,17 @@
+#' @export
+"+.fy" <- function(x, h) {
+  if (length(h) == 1L || length(h) == length(x)) {
+    if (is.integer(h)) {
+      out <- yr2fy(fy2yr(x) + h)
+    } else if (is.double(h)) {
+      if (!all(h == as.integer(h), na.rm = TRUE)) {
+        warning("`h = ", deparse(substitute(h)), " was not integerish.")
+        h <- as.integer(h)
+      }
+      out <- yr2fy(fy2yr(x) + h)
+    }
+  } else {
+    stop("`length(h) = ", length(h), "`, yet the length of `x` is ",
+         length(x), "`.")
+  }
+}
