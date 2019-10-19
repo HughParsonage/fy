69039fa9d4353ca5ec1679da4fd6574989031b82
HughParsonage <hugh.parsonage@gmail.com>
2019-10-19 20:22:25 +1100

    Harden against list input

--- R/fy_year.R
+++ R/fy_year.R
@@ -124,6 +124,11 @@ all_fy <- function(x, permitted = NULL) {
 }
 
 range_fy2yr <- function(x) {
+  # Put before any fmatch
+  if (!is.atomic(x)) {
+    stop("`x` was class ", paste0(class(x), collapse = " "),
+         " but must be atomic.")
+  }
   if (length(x) == 1L) {
     y <- fmatch(x, fys1901) + 1900L
     return(rep(y, times = 2L))
@@ -132,6 +137,8 @@ range_fy2yr <- function(x) {
       !is.null(g_max_yr <- attr(x, "fy_max_yr"))) {
     return(c(g_min_yr, g_max_yr))
   }
+
+
   y <- fmatch(x, fys1901)
 
   miny <- min(y, na.rm = TRUE) + 1900L
@@ -158,6 +165,10 @@ yr2fy <- function(yr_ending,
                   assume1901_2100 = .getOption("fy.assume1901_2100",
                                                .getOption("grattan.assume1901_2100",
                                                           TRUE))) {
+  if (!is.atomic(yr_ending)) {
+    stop("`yr_ending` was class ", paste0(class(yr_ending), collapse = " "),
+         " but must be atomic.")
+  }
   out <-
     if (assume1901_2100 ||
         AND(min(yr_ending) > 1900L,
@@ -174,6 +185,10 @@ yr2fy <- function(yr_ending,
 #' @rdname is_fy
 #' @export .yr2fy
 .yr2fy <- function(yr_ending) {
+  if (!is.atomic(yr_ending)) {
+    stop("`yr_ending` was class ", paste0(class(yr_ending), collapse = " "),
+         " but must be atomic.")
+  }
   out <-
     if (length(yr_ending) > 10e3L) {
       # Apparently quicker for > 1000
@@ -189,6 +204,11 @@ yr2fy <- function(yr_ending,
 #' @export fy2yr
 fy2yr <- function(x, na_error = TRUE) {
   check_TF(na_error)
+  if (!is.atomic(x)) {
+    stop("`x` was class ", paste0(class(x), collapse = " "),
+         " but must be atomic.")
+  }
+
   out <- 1900L + fmatch(x, fys1901)
   if (na_error && anyNA(out)) {
     if (!all(is_fy(x))) {
